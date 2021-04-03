69bbd8e6188b0018fc4f19315562c8cca7b1556e
bentrueman <40065050+bentrueman@users.noreply.github.com>
2021-04-03 17:50:26 -0300

    edits to documentation

--- R/integrate_peak.R
+++ R/integrate_peak.R
@@ -1,15 +1,18 @@
 
 #' Integrate FFF peaks
 #'
-#' @param x numeric
-#' @param y numeric
-#' @param injvol numeric
-#' @param flowrate numeric
+#' @param x A numeric vector of retention times, in minutes.
+#' @param y A numeric vector of detector responses, matching x in length.
+#' @param injvol The injection volume in litres.
+#' @param flowrate The detector flow rate in litres per minute.
 #'
-#' @return
+#' @return A number representing the integrated peak area.
 #' @export
 #'
 #' @examples
+#' x <- seq(0, 10, by = .1)
+#' y <- exp(-(x - 5) ^ 2 / 2) # gaussian peak
+#' integrate_peak(x, y)
 integrate_peak <- function(x, y, injvol = 0.001, flowrate = 0.001) {
 
   vol_per_pt <- mean(diff(x), na.rm = TRUE) * flowrate # in L
