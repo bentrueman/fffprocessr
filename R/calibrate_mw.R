0d860ac725aedd6126d87e8b9afe0def708162d0
bentrueman <40065050+bentrueman@users.noreply.github.com>
2021-04-03 23:30:15 -0300

    function documentation

--- R/calibrate_mw.R
+++ R/calibrate_mw.R
@@ -3,17 +3,33 @@
 #'
 #' @param time A numeric vector of retention times.
 #' @param mw A numeric vector of molecular weights.
-#' @param type "linear" or "quadratic"
+#' @param type Chosse from a "linear" or "quadratic" fit.
 #' @param newdata A vector of retention times or molecular weights.
-#' @param output "time" or "mw"
+#' @param output Choose the output type: "time" for retention time or "mw" for molecular weight.
+#' Output "time" is an inverse prediction useful primarily for plotting.
 #' @param predict Logical. Predict from the model?
 #'
-#' @return
+#' @return An object of class "lm" if predict = FALSE, or a vector of model predictions/inverse
+#' predictions if predict = TRUE.
 #' @importFrom dplyr %>%
 #' @export
 #'
 #' @examples
-calibrate_mw <- function(time, mw, type = "quadratic", newdata = NULL, output = "time", predict = TRUE) {
+#' retention <- c(11.35890, 19.49773, 19.24173, 13.88310, 18.73001, 17.67315)
+#' mw_kda <- c(0.69, 2000.00, 440.00, 1.80, 158.00, 44.00)
+#' # return tick positions for 1, 10, 100, and 1000 kDa,
+#' # based on a quadratic calibration curve:
+#' calibrate_mw(retention, mw_kda, newdata = c(1, 10, 100, 1000), predict = TRUE)
+calibrate_mw <- function(
+  time,
+  mw,
+  type = "quadratic",
+  newdata = NULL,
+  output = "time",
+  predict = TRUE
+) {
+
+  if(predict & is.null(newdata)) stop("Argument 'newdata' is missing with no default.")
 
   model <- if(type == "quadratic") {
     stats::lm(log10(mw) ~ time + I(time ^ 2))
