084c7b4eb050a4f25e49c9935b1d547d4884481d
bentrueman <40065050+bentrueman@users.noreply.github.com>
2021-06-21 11:24:06 -0300

    more tests

--- tests/testthat/test-combine_fff.R
+++ tests/testthat/test-combine_fff.R
@@ -5,3 +5,25 @@ test_that("number of rows is as expected", {
     nrow(combine_fff(load_icp(path), load_uv(path, UV254_1))), 11872
   )
 })
+
+
+missing_blank <- tidyr::crossing(
+  date = c("2021-01-01", "2021-02-01"),
+  sample = c("blank", "sample_1"),
+  param = letters[1:2],
+  time = 1:2
+) %>%
+  tidyr::unite(file, date, sample, param, remove = FALSE) %>%
+  dplyr::mutate(conc = runif(nrow(.))) %>%
+  dplyr::filter(!(sample == "blank" & date == "2021-02-01"))
+
+test_that("combine_fff() handles missing blank runs", {
+  expect_equal(
+    nrow(
+      dplyr::distinct(
+        combine_fff(uv = missing_blank, subtract_blank = TRUE, focus = 1),
+        date, param
+      )
+    ), 4
+  )
+})
