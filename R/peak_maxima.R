03395d2c53713cf694d4d938cf893738263ce200
bentrueman <40065050+bentrueman@users.noreply.github.com>
2021-06-22 17:26:34 -0300

    new method for peak_maxima()

--- R/peak_maxima.R
+++ R/peak_maxima.R
@@ -7,6 +7,9 @@
 #' @param peaks Number of peaks.
 #' @param n Before detecting peak minima, compute an n-point moving average of the detector response using
 #' `stats::filter(x, filter = rep(1/n, n))`.
+#' @param method Algorithm for peak detection. Either "gam", which separates peaks by the minima between them, or "sigma" which recursively
+#' calculates the standard deviation by eliminating values greater than three standard deviations.
+#' @param max_iter Maximum iterations for method = "sigma".
 #'
 #' @return A tibble of peak retention times and the corresponding maxima in detector response.
 #' @importFrom dplyr %>%
@@ -18,14 +21,31 @@
 #' data <- combine_fff(load_icp(path))
 #' data <- data[data$param == "65Cu", ]
 #' peak_maxima(data, peaks = 3)
-peak_maxima <- function(data, focus = 10, k = 35, peaks = 1, n = 1) {
-  data %>%
-    dplyr::filter(.data$time > focus) %>%
+peak_maxima <- function(data, focus = 10, k = 35, peaks = 1, n = 1, method = "gam", max_iter = 20) {
+
+  data <- data %>%
     dplyr::group_by(.data$date, .data$sample, .data$param) %>%
     dplyr::mutate(
       conc = stats::filter(.data$conc, rep(1/n, n)) %>%
         as.numeric() %>%
-        imputeTS::na_interpolation(),
+        imputeTS::na_interpolation()
+    )
+
+  if(method == "gam") peak_id_gam(data, focus, k, peaks) else
+    if(method == "sigma") {
+      data %>%
+        tidyr::nest() %>%
+        dplyr::ungroup() %>%
+        dplyr::mutate(peaks = purrr::map(.data$data, ~ peak_id_sigma(.x, focus, peaks, max_iter))) %>%
+        tidyr::unnest(.data$peaks) %>%
+        dplyr::select(-.data$data)
+    } else stop("choose a valid method: 'gam' or 'sdev'")
+}
+
+peak_id_gam <- function(data, focus, k, peaks) {
+  data %>%
+    dplyr::filter(.data$time > focus) %>%
+    dplyr::mutate(
       fitted = mgcv::gam(.data$conc ~ s(.data$time, bs = "cs", k = k)) %>%
         mgcv::predict.gam(),
       diff = dplyr::lead(.data$fitted) - .data$fitted, # slope
@@ -43,46 +63,67 @@ peak_maxima <- function(data, focus = 10, k = 35, peaks = 1, n = 1) {
     dplyr::ungroup()
 }
 
-# sketch of an alternate method:
+peak_id_sigma <- function(data, focus, peaks, max_iter) {
 
-# # this groups each cluster:
-#
-# group_peaks <- function(x) {
-#   x <- c(0, x) # pad x with a zero so that x[i-1] exists when i=1
-#   g <- rep(NA_real_, length(x))
-#   counter <- 0
-#   for(i in 2:length(x)) {
-#     if(x[i] == 0) {
-#       g[i] <- 0
-#     } else if(x[i] == 1 & x[i] == x[i-1]) {
-#       g[i] <- counter
-#     } else {
-#       counter <- counter + 1
-#       g[i] <- counter
-#     }
-#   }
-#   g[-1]
-# }
-#
-# icp %>%
-#   correct_baseline(10, 37) %>%
-#   filter(param == "56Fe", sample == "sample_bennery_raw") %>%
-#   mutate(
-#     sd = sd(conc),
-#     peak = conc > 3 * sd,
-#     sd = sd(conc[!peak]),
-#     peak = conc > 3 * sd,
-#     g = group_peaks(peak)
-#   ) %>%
-#   ggplot(aes(time, conc, col = as.numeric(peak))) +
-#   geom_point(
-#     data = function(x) x %>%
-#       group_by(g) %>%
-#       summarize(
-#         time = time[which.max(conc)],
-#         conc = max(conc)
-#       ) %>%
-#       filter(g != 0),
-#     aes(time, conc), inherit.aes = FALSE
-#   ) +
-#   geom_line()
+  peak_list <- list()
+  peak_region <- rep(FALSE, nrow(data))
+  iter <- 0
+  peak_id <- 0
+
+  while(peak_id < peaks & iter < max_iter) {
+
+    iter <- iter + 1
+
+    sdev <- tidyr::replace_na(stats::sd(data$conc[!peak_region]), 0)
+
+    data <- data %>%
+      dplyr::mutate(
+        peak_region = .data$conc > 3 * sdev,
+        g = group_peaks(peak_region)
+      )
+
+    peak_region <- data$peak_region
+
+    peak_list[[iter]] <- list(
+      data %>%
+        dplyr::group_by(.data$g) %>%
+        dplyr::summarize(
+          conc_tr = max(.data$conc),
+          tr = .data$time[which.max(.data$conc)]
+        ) %>%
+        dplyr::filter(.data$g != 0)
+    )
+
+    peak_tbl <- dplyr::bind_rows(peak_list) %>%
+      dplyr::select(-.data$g) %>%
+      dplyr::distinct() %>%
+      dplyr::arrange(.data$tr) %>%
+      dplyr::filter(.data$tr > focus)
+
+    peak_id <- nrow(peak_tbl)
+  }
+
+  peak_tbl %>%
+    dplyr::slice(1:peaks)
+
+}
+
+group_peaks <- function(x) {
+
+  x <- c(0, x) # pad x with a zero so that x[i-1] exists when i=1
+  g <- rep(NA_real_, length(x))
+  counter <- 0
+
+  for(i in 2:length(x)) {
+    if(x[i] == 0) {
+      g[i] <- 0
+    } else if(x[i] == 1 & x[i] == x[i-1]) {
+      g[i] <- counter
+    } else {
+      counter <- counter + 1
+      g[i] <- counter
+    }
+  }
+
+  g[-1]
+}
--- tests/testthat/test-peak_maxima.R
+++ tests/testthat/test-peak_maxima.R
@@ -1,7 +1,18 @@
+
+path <- system.file("extdata", package = "fffprocessr")
+data <- combine_fff(load_icp(path))
+data <- data[data$param == "65Cu", ]
+
 test_that("peak_maxima() yields the expected peaks", {
-  path <- system.file("extdata", package = "fffprocessr")
-  data <- combine_fff(load_icp(path))
-  data <- data[data$param == "65Cu", ]
   maxima <- peak_maxima(data, peaks = 3)
   expect_equal(unique(round(maxima$tr)), c(12, 14, 20, 21))
 })
+
+test_that("peak_maxima() methods equivalent for example data", {
+  expect_equal(
+    round(peak_maxima(data, peaks = 3, method = "gam")$tr),
+    round(peak_maxima(data, peaks = 3, method = "sigma", n = 7)$tr)
+  )
+})
+
+
