fdf4bdd0c4764a3629a5e2ea9e678bc294a55c04
bentrueman <40065050+bentrueman@users.noreply.github.com>
2021-06-24 13:31:33 -0300

    more tests

--- tests/testthat/test-correct_baseline.R
+++ tests/testthat/test-correct_baseline.R
@@ -1,15 +1,28 @@
-test_that("correct_baseline() works with group_vars = NULL and alternate x and y variable names", {
 
-  data <- data.frame(
-    test_x = seq(0, 30, by = .1),
-    test_y = stats::runif(301, 0, 1)
-  )
-  data$test_y <- data$test_y + data$test_x
+data <- data.frame(
+  x = "a",
+  test_x = seq(0, 30, by = .1),
+  test_y = stats::runif(301, 0, 1)
+)
+data$test_y <- data$test_y + data$test_x
+
+test_that("correct_baseline() works with group_vars = NULL and alternate x and y variable names", {
 
   expect_equal(
     ncol(correct_baseline(
       data, left = .2, right = 29.8,
       group_vars = NULL, x_var = "test_x", y_var = "test_y"
+    )),
+    2
+  )
+})
+
+test_that("correct_baseline() works with group_vars = NULL and alternate x and y variable names", {
+
+  expect_equal(
+    ncol(correct_baseline(
+      data, left = .2, right = 29.8,
+      group_vars = "x", x_var = "test_x", y_var = "test_y"
     )),
     2
   )
--- tests/testthat/test-peak_maxima.R
+++ tests/testthat/test-peak_maxima.R
@@ -15,16 +15,37 @@ test_that("peak_maxima() methods equivalent for example data", {
   )
 })
 
+tesdat <- tibble::tibble(
+  x = "a",
+  test_x = 1:100,
+  test_y = withr::with_seed(101, {stats::rlnorm(length(x), sdlog = 3)})
+)
+
 test_that("peak_maxima() works with group_vars = NULL and alternate x and y variable names", {
 
-  tesdat <- tibble::tibble(
-    g = "a",
-    x = 1:100,
-    y = withr::with_seed(101, {stats::rlnorm(length(x), sdlog = 3)})
-  )
-
   expect_equal(
-    ncol(peak_maxima(tesdat, group_vars = NULL, x_var = "x", y_var = "y", method = "sigma")),
+    ncol(peak_maxima(tesdat, group_vars = NULL, x_var = "test_x", y_var = "test_y", method = "sigma")),
     3
   )
 })
+
+test_that("peak_maxima() works with group_vars equal to an internal variable name", {
+  expect_equal(
+    ncol(peak_maxima(tesdat, group_vars = "x", x_var = "test_x", y_var = "test_y", method = "sigma")),
+    4
+  )
+})
+
+test_that("peak_maxima() handles NA", {
+  tesdat$test_y[4] <- NA_real_
+  expect_equal(
+    ncol(peak_maxima(tesdat, group_vars = "x", x_var = "test_x", y_var = "test_y", method = "sigma")),
+    4
+  )
+})
+
+test_that("peak_maxima() orders by peak height and x to reach number of prespecified peaks", {
+  expect_equal(FALSE, TRUE)
+})
+
+
